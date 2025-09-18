933c0083ff3dbee20f6c2c3c54ccbdaa602a9058
Edgar Rosario <edg507@outlook.com>
2025-09-17 19:57:20 -0500

    ejercicio2

---
+++ Laboratorio3/Laboratorio32/Laboratorio32/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        double a;
+
+        Console.WriteLine("Introduce el Primer Numero: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        /* Console.WriteLine("Introduce el segundo numero:");
+         b = Convert.ToInt32(Console.ReadLine());*/
+
+        double resultado = CalculosMatematicos.CalculoArea(a);
+
+        Console.WriteLine("Este es el calculo matematico {0}", resultado);
+
+    }
+
+    public class CalculosMatematicos
+    {
+
+        /* public static int Calcular(int a, int b)
+         {
+             int calcular = (a + b) * (a - b);
+             return calcular;
+         }
+         */
+
+        public static double CalculoArea(double a)
+        {
+            const Double PI = 3.14;
+            double radio = a;
+
+            return PI * (radio*radio);
+        }
+
+    }
+
+}
