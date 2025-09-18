beb281e4cfd235ad0660299ed5d98e27c835ba9f
Edgar Rosario <edg507@outlook.com>
2025-09-17 19:26:23 -0500

    ejercicio

---
+++ Laboratorio3/Laboratorio31/Laboratorio31/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+
+
+
+        Console.WriteLine("Introduce el Primer Numero: ");
+        a=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo numero:");
+        b=Convert.ToInt32(Console.ReadLine());
+
+        double resultado = CalculosMatematicos.Calcular(a, b);
+
+        Console.WriteLine("Este es el calculo matematico {0}" ,resultado );
+
+    }
+
+    public class CalculosMatematicos
+    {
+
+        public static int Calcular(int a, int b) 
+        {
+            int calcular= (a + b) * (a - b);
+            return calcular;
+        }    
+    
+    }
+
+}
