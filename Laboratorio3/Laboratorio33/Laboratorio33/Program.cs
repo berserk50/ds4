dea03af9ecbadb3f06d6ad5f67be85b8dcbc54bb
Edgar Rosario <edg507@outlook.com>
2025-09-17 21:19:55 -0500

    ejercicio3

---
+++ Laboratorio3/Laboratorio33/Laboratorio33/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int altura, ancho,perimetro;
+
+        Console.WriteLine("Ingrese la altura del rectangulo ");
+        altura = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el ancho del rectangulo");
+        ancho= Convert.ToInt32(Console.ReadLine());
+
+        perimetro = CalculoMatematico.CalcularPerimetro(altura, ancho);
+
+        Console.WriteLine("El perimetro del rectangulo es {0}", perimetro);
+    }
+
+    public class CalculoMatematico
+    {
+
+        public static int CalcularPerimetro(int altura, int ancho) 
+        {
+
+            return 2 * (altura + ancho);
+        
+        
+        }
+    
+    
+    }
+}
