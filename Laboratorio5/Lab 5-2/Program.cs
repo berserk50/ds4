ee85742d833373ec01268e418476da3ab86ae165
Edgar Rosario <edg507@outlook.com>
2025-09-23 19:17:57 -0500

    Lab 5-2

---
+++ Laboratorio5/Lab 5-2/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+
+    private int[,] mat;
+
+    public void Ingresar()
+    {
+        mat = new int[3, 4];
+        for (int f = 0; f < 3; f++)
+        {
+
+            for (int c = 0; c < 3; c++)
+            {
+                Console.Write("Ingrese posicion[" + (f + 1) + "," + (c + 1) + "]:")
+                string linea;
+                linea = Console.ReadLine();
+                mat[f, c] = int.Parse(linea);
+
+            }
+        }
+    }
+}
