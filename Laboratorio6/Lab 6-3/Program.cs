ec7e77ca3ac6223cc7a5a3365be234cb49aa4059
Edgar Rosario <edg507@outlook.com>
2025-09-23 20:04:20 -0500

    Lab 6-3

---
+++ Laboratorio6/Lab 6-3/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumber = { 1, 2, 3 };
+            Console.WriteLine(myNumber[10]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal, valido el indice del arreglo");
+
+        }
+        finally
+        {
+            Console.WriteLine("Continuacion de la aplicacion, luego del bloque try/catch");
+        }
+    }
+}
