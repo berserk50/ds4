09d8b3000f92f6abc445477802c69b48882280b9
Edgar Rosario <edg507@outlook.com>
2025-09-28 12:19:25 -0500

    lab 7.2

---
+++ Laboratorio7/Lab7.2/Dado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._2
+{
+    internal class Dado
+    {
+        private int valor;
+        private static Random aleatorio;
+
+        public Dado()
+        {
+            aleatorio = new Random();
+        }
+
+        public void Tirar() { 
+        valor =aleatorio.Next(1,7);
+        
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dados es:" + valor);
+        }
+
+        public int RetornarValor() { 
+            return valor;
+        }
+    }
+}
---
+++ Laboratorio7/Lab7.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lab7._2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        JuegoDeDadis j = new JuegoDeDadis();
+
+        j.Jugar();
+    }
+}
