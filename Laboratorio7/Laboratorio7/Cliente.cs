4b79687131ec7e025d84ae0cbdfee36e685ee0f4
Edgar Rosario <edg507@outlook.com>
2025-09-28 12:08:38 -0500

    lab 7.1

---
+++ Laboratorio7/Laboratorio7/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    class Cliente
+    {
+        private string nombre;
+        private int monto;
+        
+        public Cliente(string nom)
+        {
+            nombre = nom;
+            monto = 0;
+    
+        }
+
+        public void Depoisitar(int m)
+        {
+            monto = monto + m;
+
+        }
+
+        public void Extraer (int m)
+        { monto = monto - m; }
+
+        public int RetornarMonto()
+        { return monto; }
+
+    public void Imprimir()
+    { Console.WriteLine(nombre + "Tiene depositado la suma de " + monto); }
+    }
+}
