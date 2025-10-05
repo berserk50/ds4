561b43e088886e0777c72ef3fe6618e8d96f8bfb
Edgar Rosario <edg507@outlook.com>
2025-10-05 16:40:08 -0500

    Lab94

---
+++ Laboratorio9/Lab94/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab94;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Aleatorios aleatorio = new Aleatorios();
+        int numeroGenerado = aleatorio.GenerarNumero();
+        Console.WriteLine(numeroGenerado);
+
+
+        int[] arreglo= aleatorio.GenerarArreglo();
+        Console.WriteLine(arreglo.Length);
+    }
+}
