97393cae45c9db590cecdba03323fd84a000253a
Edgar Rosario <edg507@outlook.com>
2025-11-28 11:08:59 -0500

    Lab19 finales

---
+++ Laboratorio 19/Aplicacion19-3/Controllers/ValueController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicacion19_3.Controllers
+{
+    public class ValueController : Controller
+    {
+        public ActionResult Index()
+        {
+            var url = "https://localhost:44396/item/{id}";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";   
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null)
+                            return View();
+
+                        using (StreamReader objReader = new StreamReader(strReader)) 
+                        {
+                            string responseBody = objReader.ReadToEnd();
+
+                            ViewBag.Data = responseBody;   
+                        }
+                    }
+                }
+            }
+            catch (WebException )
+            {
+            }
+            return View();
+        }
+
+    }
+}
--- Laboratorio 19/Laboratorio 19-1/Controllers/ValuesController.cs
+++ Laboratorio 19/Laboratorio 19-1/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace Laboratorio_19_1.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
